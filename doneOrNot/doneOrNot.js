5828d480ff6909ce43699db029d9bac657e6e748
ivanmauricio <ivangh@hotmail.co.uk>
2015-11-12 12:46:48 +0000

    Change var names and added comments
    changed some var names to make more code more descriptive and added a
    few comments. also changed the way in which array checker validates
    array by adding validArray var

--- doneOrNot/doneOrNot.js
+++ doneOrNot/doneOrNot.js
@@ -1,18 +1,25 @@
-function doneOrNot(board){
-  var result = [];
-  function arrayChecker(array) {
+function doneOrNot (board) {
+  
+  var originalBoard = board.slice(); // makes a clone of orginal board so that it is not modified by our functions
+  
+  // checks whether an array contains each of the values 1-9 once
+  var validArray = true;
+  function arrayChecker (array) {
     var orderedString = [1, 2, 3, 4, 5, 6, 7, 8, 9].toString();
     var arrayString = array.sort(function(a,b) { return a-b; }).toString();
-    if(orderedString != arrayString)
-      result.push(1);  
+    if (orderedString !== arrayString) {
+      validArray = false;
+    }
   }
 
-  var board2 = [[],[],[],[],[],[],[],[],[]];
+  // makes a new board rotated by 90 degrees so that columns can be validated
+  var rotatedBoard = [[],[],[],[],[],[],[],[],[]];
     for(var i = 0; i < 9; i++) {
         for(var j = 0; j < 9; j++){
-            board2[i].push(board[j][i]);
+            rotatedBoard[i].push(board[j][i]);
         }
     }
+  
    var longArray = board.reduce(function(a, b) { return a.concat(b); });
    var board3 = [[],[],[],[],[],[],[],[],[]];
 
@@ -29,3 +36,23 @@ function doneOrNot(board){
      return 'Try again!';
    return 'Finished!';  
 }
+
+doneOrNot([[5, 3, 4, 6, 7, 8, 9, 1, 2], 
+           [6, 7, 2, 1, 9, 5, 3, 4, 8],
+           [1, 9, 8, 3, 4, 2, 5, 6, 7],
+           [8, 5, 9, 7, 6, 1, 4, 2, 3],
+           [4, 2, 6, 8, 5, 3, 7, 9, 1],
+           [7, 1, 3, 9, 2, 4, 8, 5, 6],
+           [9, 6, 1, 5, 3, 7, 2, 8, 4],
+           [2, 8, 7, 4, 1, 9, 6, 3, 5],
+           [3, 4, 5, 2, 8, 6, 1, 7, 9]]);
+                                
+doneOrNot([[5, 3, 4, 6, 7, 8, 9, 1, 2], 
+           [6, 7, 2, 1, 9, 0, 3, 4, 9],
+           [1, 0, 0, 3, 4, 2, 5, 6, 0],
+           [8, 5, 9, 7, 6, 1, 0, 2, 0],
+           [4, 2, 6, 8, 5, 3, 7, 9, 1],
+           [7, 1, 3, 9, 2, 4, 8, 5, 6],
+           [9, 0, 1, 5, 3, 7, 2, 1, 4],
+           [2, 8, 7, 4, 1, 9, 6, 3, 5],
+           [3, 0, 0, 4, 8, 1, 1, 7, 9]]);
